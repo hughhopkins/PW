21bc65efb8d1cbebe12bd3ac954dd1aa374beed4
Hugh Hopkins <hugh.hopkins@gmail.com>
2026-08-25 11:40:02 +0100

    Add partial password output masking

--- macOS/PWTests/PWTests.swift
+++ macOS/PWTests/PWTests.swift
@@ -11,6 +11,20 @@ import XCTest
 
 class PWTests: XCTestCase {
 
+    // MARK: - Password output privacy
+
+    func testPartiallyMaskedOutput_hidesLastThreeQuarters() {
+        let password = "0123456789abcdefghijklmnopqrstuvwxyzABCD"
+        let displayed = PasswordOutputDisplay.partiallyMasked(password)
+
+        XCTAssertEqual(displayed, "0123456789" + String(repeating: "•", count: 30))
+        XCTAssertEqual(displayed.count, password.count)
+    }
+
+    func testPartiallyMaskedOutput_emptyPasswordStaysEmpty() {
+        XCTAssertEqual(PasswordOutputDisplay.partiallyMasked(""), "")
+    }
+
     // MARK: - V1 Tests
 
     func testV1_facebookHackference() {
